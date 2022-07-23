a0b008cf81a918db81f4747abe903d6112a220a4
ShinjungOh <tlswjd9260@gmail.com>
2022-07-23 20:21:08 +0900

    [layout] feat: GlobalStyles 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import GlobalStyles from '../ui/core/GlobalStyles';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>어떤 하루</title>
       </Head>
+      <GlobalStyles />
       <Component {...pageProps} />
     </>
   );
